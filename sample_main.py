67825a6139c3ca54625bcbb9e3439fb2e49ca115
wangdaniel0205 <wangdaniel0205@gmail.com>
2021-06-23 10:35:33 +0900

    06/23

--- sample_main.py
+++ sample_main.py
@@ -9,4 +9,15 @@ if __name__ == '__main__':
     solution = Solution()
     for case in testCases:
         ListNode.printAll(solution.mergeTwoLists(case[0], case[1]))
+
+
+
+if __name__ == '__main__':
+    testCases = [
+        [[4,5,6,7,0,1,2], 0]
+    ]
+
+    solution = Solution()
+    for case in testCases:
+        print(solution.search(case[0], case[1]))
 '''
