12f01ebd24d69b1a7cb00728c8191a9d7edf4554
wangdaniel0205 <wangdaniel0205@gmail.com>
2021-08-12 22:23:43 +0900

    08/12

--- draft.py
+++ draft.py
@@ -4,17 +4,6 @@ count = Counter(candidate)
 group = [[k, count[k]] for k in count.keys()]
 print(group)
 # %%
-s = "eat"
-l = []
-for c in s:
-    i = 0
-    for k in l:
-        if k >= c:
-            break
-        i += 1
-    l.insert(i,c)
-print(hash(tuple(l)))
-# %%
-h = {'a':[1], 'b':[2]}
-print(list(h.values()))
+k = [0,1,2]
+k.ins
 # %%
